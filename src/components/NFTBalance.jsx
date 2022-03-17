0c680bde58b5fda2631b01b7304bb4dbb49b29b7
Bryce Palichuk <43261252+Brycepalichuk@users.noreply.github.com>
2022-03-17 10:36:30 -0600

    Update Your Collection Tab

--- src/components/NFTBalance.jsx
+++ src/components/NFTBalance.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { useMoralis } from "react-moralis";
-import { Card, Image, Tooltip, Modal, Input, Alert, Spin, Button } from "antd";
+import { useMoralisWeb3Api } from "react-moralis";
+import { Card, Image, Tooltip, Modal, Alert } from "antd"; // Input, Spin, Button
 import { useNFTBalance } from "hooks/useNFTBalance";
-import { FileSearchOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { FileSearchOutlined, DownloadOutlined } from "@ant-design/icons";
 import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
 import { getExplorer } from "helpers/networks";
-import { useWeb3ExecuteFunction } from "react-moralis";
+
 const { Meta } = Card;
 
 const styles = {
@@ -22,147 +22,50 @@ const styles = {
 
 function NFTBalance() {
   const { NFTBalance, fetchSuccess } = useNFTBalance();
-  const { chainId, marketAddress, contractABI } = useMoralisDapp();
-  const { Moralis } = useMoralis();
+  const { chainId } = useMoralisDapp();
   const [visible, setVisibility] = useState(false);
-  const [nftToSend, setNftToSend] = useState(null);
-  const [price, setPrice] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const contractProcessor = useWeb3ExecuteFunction();
-  const contractABIJson = JSON.parse(contractABI);
-  const listItemFunction = "createMarketItem";
-  const ItemImage = Moralis.Object.extend("ItemImages");
+  const [nftToGrabFiles, setNftToGrabFiles] = useState(null);
+  const Web3Api = useMoralisWeb3Api();
 
-  async function list(nft, listPrice) {
-    setLoading(true);
-    const p = listPrice * ("1e" + 18);
-    const ops = {
-      contractAddress: marketAddress,
-      functionName: listItemFunction,
-      abi: contractABIJson,
-      params: {
-        nftContract: nft.token_address,
-        tokenId: nft.token_id,
-        price: String(p),
-      },
+  async function fetchNFTMetadata(nft) {
+    const options = {
+      chain: chainId,
+      address: nft.token_address,
+      token_id: nft.token_id
     };
+    const metaData = await Web3Api.token.getTokenIdMetadata(options)
+    const metadata = JSON.parse(metaData.metadata);
+    const attributes = metadata.attributes
+    // console.log(metadata)
 
-    await contractProcessor.fetch({
-      params: ops,
-      onSuccess: () => {
-        console.log("success");
-        setLoading(false);
-        setVisibility(false);
-        addItemImage();
-        succList();
-      },
-      onError: (error) => {
-        setLoading(false);
-        failList();
-      },
-    });
+    let files;
+    
+    for (let i = 0; i < attributes.length - 1; i++) {
+      let hash = '';
+      let name = '';
+      name = attributes[i].value
+      hash = attributes[i].file
+      let ipfsHash = hash.replace("ipfs://", "")
+      const url = `https://ipfs.moralis.io:2053/ipfs/${ipfsHash}`
+      const response = await fetch(url).then(r => r.blob())
+      var fileDownload = require('js-file-download');
+      fileDownload(response, name + ".stl")
+      // console.log(response)
+      
+    }
+    
+    console.log(files)
+
   }
 
-
-  async function approveAll(nft) {
-    setLoading(true);  
-    const ops = {
-      contractAddress: nft.token_address,
-      functionName: "setApprovalForAll",
-      abi: [{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"}],
-      params: {
-        operator: marketAddress,
-        approved: true
-      },
-    };
-
-    await contractProcessor.fetch({
-      params: ops,
-      onSuccess: () => {
-        console.log("Approval Received");
-        setLoading(false);
-        setVisibility(false);
-        succApprove();
-      },
-      onError: (error) => {
-        setLoading(false);
-        failApprove();
-      },
-    });
-  }
-
-  const handleSellClick = (nft) => {
-    setNftToSend(nft);
+  const handleDownloadFileClick = (nft) => {
+    setNftToGrabFiles(nft);
     setVisibility(true);
-  };
-
-  function succList() {
-    let secondsToGo = 5;
-    const modal = Modal.success({
-      title: "Success!",
-      content: `Your NFT was listed on the marketplace`,
-    });
-    setTimeout(() => {
-      modal.destroy();
-    }, secondsToGo * 1000);
-  }
-
-  function succApprove() {
-    let secondsToGo = 5;
-    const modal = Modal.success({
-      title: "Success!",
-      content: `Approval is now set, you may list your NFT`,
-    });
-    setTimeout(() => {
-      modal.destroy();
-    }, secondsToGo * 1000);
-  }
-
-  function failList() {
-    let secondsToGo = 5;
-    const modal = Modal.error({
-      title: "Error!",
-      content: `There was a problem listing your NFT`,
-    });
-    setTimeout(() => {
-      modal.destroy();
-    }, secondsToGo * 1000);
-  }
-
-  function failApprove() {
-    let secondsToGo = 5;
-    const modal = Modal.error({
-      title: "Error!",
-      content: `There was a problem with setting approval`,
-    });
-    setTimeout(() => {
-      modal.destroy();
-    }, secondsToGo * 1000);
-  }
-
-  function addItemImage() {
-    const itemImage = new ItemImage();
-
-    itemImage.set("image", nftToSend.image);
-    itemImage.set("nftContract", nftToSend.token_address);
-    itemImage.set("tokenId", nftToSend.token_id);
-    itemImage.set("name", nftToSend.name);
-
-    itemImage.save();
   }
 
   return (
     <>
       <div style={styles.NFTs}>
-        {contractABIJson.noContractDeployed && (
-          <>
-            <Alert
-              message="No Smart Contract Details Provided. Please deploy smart contract and provide address + ABI in the MoralisDappProvider.js file"
-              type="error"
-            />
-            <div style={{ marginBottom: "10px" }}></div>
-          </>
-        )}
         {!fetchSuccess && (
           <>
             <Alert
@@ -171,7 +74,7 @@ function NFTBalance() {
             />
             <div style={{ marginBottom: "10px" }}></div>
           </>
-        )}
+        )} 
         {NFTBalance &&
           NFTBalance.map((nft, index) => (
             <Card
@@ -181,14 +84,14 @@ function NFTBalance() {
                   <FileSearchOutlined
                     onClick={() =>
                       window.open(
-                        `${getExplorer(chainId)}address/${nft.token_address}`,
+                        `${getExplorer(chainId)}token/${nft.token_address}?a=${nft.token_id}#inventory`,
                         "_blank"
                       )
                     }
                   />
                 </Tooltip>,
-                <Tooltip title="List NFT for sale">
-                  <ShoppingCartOutlined onClick={() => handleSellClick(nft)} />
+                <Tooltip title="Download Files, if available">
+                  <DownloadOutlined onClick={() => handleDownloadFileClick(nft)} />
                 </Tooltip>,
               ]}
               style={{ width: 240, border: "2px solid #e7eaf3" }}
@@ -203,46 +106,18 @@ function NFTBalance() {
               }
               key={index}
             >
-              <Meta title={nft.name} description={nft.contract_type} />
+              <Meta title={nft.name} description={nft.contract_type}/>
             </Card>
           ))}
       </div>
-      <Modal
-        title={`List ${nftToSend?.name} #${nftToSend?.token_id} For Sale`}
-        visible={visible}
-        onCancel={() => setVisibility(false)}
-        onOk={() => list(nftToSend, price)}
-        okText="List"
-        footer={[
-          <Button onClick={() => setVisibility(false)}>
-            Cancel
-          </Button>,
-          <Button onClick={() => approveAll(nftToSend)} type="primary">
-            Approve
-          </Button>,
-          <Button onClick={() => list(nftToSend, price)} type="primary">
-            List
-          </Button>
-        ]}
-      >
-        <Spin spinning={loading}>
-          <img
-            src={`${nftToSend?.image}`}
-            alt=""
-            style={{
-              width: "250px",
-              margin: "auto",
-              borderRadius: "10px",
-              marginBottom: "15px",
-            }}
-          />
-          <Input
-            autoFocus
-            placeholder="Listing Price in MATIC"
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </Spin>
-      </Modal>
+        <Modal
+          title={`Download ${nftToGrabFiles?.name} #${nftToGrabFiles?.token_id} Files`}
+          visible={visible}
+          onOk={() => fetchNFTMetadata(nftToGrabFiles)}
+          onCancel={() => setVisibility(false)}
+        >
+          <p>Click OK to download the 3d printer files!</p>
+        </Modal>
     </>
   );
 }
